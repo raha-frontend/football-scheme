c816f9c4b47dcb083e813f0c83eb384262242049
Rahman <rahmangadzh1994@gmail.com>
2020-08-28 22:12:52 +0300

    change title

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ const strategyField = document.querySelector('#strategy');
 const footballField = document.querySelector('.football-field');
 
 selectBtn.addEventListener('click', function() {
-    console.log('123', strategyField);
-    console.log(strategyField.value);
-
     if (strategyField.value === '4-2-3-1') {
         footballField.classList.remove('football-field-4-3-3');
         footballField.classList.remove('football-field-4-4-2');
